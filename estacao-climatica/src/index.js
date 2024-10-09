77d617640a41613783119625e95eaf9024426157
Eric Figueiredo <Ericxtk@gmail.com>
2024-10-09 19:42:20 -0300

    ap05(estacao-climatica): encerra o codigo 2.9
    - retomar tratando erros

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -14,28 +15,83 @@ class App extends React.Component {
     }
   }
 
-	obterEstacao = (data,latitude) => {
-		const anoAtual = data.getFullYear()
-		//21/06
-		const d1 = new Date(anoAtual, 5, 21)
-		//24/09
-		const d2 = new Date(anoAtual, 8, 24)
-		//22/12
-		const d3 = new Date(anoAtual, 11,22)
-		//21/03
-		const d4 = new Date(anoAtual, 2, 21)
-		const estouNoSul = latitude < 0
-		if (data >= d1 && data < s2)
-			return estouNoSul ? 'Inverno' : 'Verão'
-		if (data >= d2 && data < d3)
-			return estouNoSul ? 'Primavera' : 'Outono'
-		if (data >= d3 || data < d4)
-			return estouNoSul ? 'Verão' : 'Inverno'
-		return estouNoSul ? 'Outono' : 'Primavera'
-	}
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //21/06
+    const d1 = new Date(anoAtual, 5, 21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21)
+    const estouNoSul = latitude < 0
+    if (data >= d1 && data < d2) return estouNoSul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3) return estouNoSul ? 'Primavera' : 'Outono'
+    if (data >= d3 || data < d4) return estouNoSul ? 'Verão' : 'Inverno'
+    return estouNoSul ? 'Outono' : 'Primavera'
+  }
+
+  icones = {
+    Primavera: 'fa-solid fa-seedling',
+    Inverno: 'fa-solid fa-snowflake',
+    Verão: 'fa-solid fa-sun',
+    Outono: 'fa-brands fa-canadian-maple-leaf'
+  }
+
+  obterLocalizacao = () => {
+    window.navigator.geolocation.getCurrentPosition(posicao => {
+      //1. construir um objeto Date que representa a data atual do sistema
+      let data = new Date()
+      //2. obter a estacao climatica usando a funcao obterEstacao
+      let estacao = this.obterEstacao(data, posicao.coords.latitude)
+      //3. obter o icone, usando o mapa que a gente acabou de fazer
+      let icone = this.icones[estacao]
+      //4. atualizar o estado do componente usando a função this.setState
+      this.setState({
+        latitude: posicao.coords.latitude,
+        longitude: posicao.coords.longitude,
+        estacao: estacao,
+        data: data,
+        icone: icone
+      })
+    })
+  }
 
   render() {
-    return <div>App</div>
+    return (
+      <div className="container p-4 border mt-2">
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-md-8">
+            <div className="card">
+              <div className="card-body">
+                <div
+                  className="d-flex align-items-center"
+                  style={{ height: '6 rem' }}
+                >
+                  <i className={`fa-5x ${this.state.icone}`}></i>
+                  <p className="w-75 ms-3 text-center fs-1">
+                    {this.state.estacao}
+                  </p>
+                </div>
+                <p className="text-center">
+                  {
+										this.state.latitude
+                    ? `Coords: ${this.state.latitude}, ${this.state.longitude}, Date: ${this.state.data}`
+                    : 'Clique no botão para saber sua estação climática'
+									}
+                </p>
+								<button
+								onClick={this.obterLocalizacao}
+								className='btn btn-outline-primary w-100 mt-2'>
+									Qual a minha estação?
+								</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
